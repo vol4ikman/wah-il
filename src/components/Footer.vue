7b69c8cf79f1032629f08e096c3af583790b5bef
Alex Volkov <vol4ikman@gmail.com>
2017-09-29 21:18:15 +0300

    29 Sep update footer added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,6 +5,38 @@
             <div class="home-banner-inner" style="background: #236478 url(https://accessibility-helper.co.il/wp-content/themes/wah-theme/images/banner-772x250.jpg) no-repeat center; height: 380px;"></div>
         </div>
 
+        <div class="section footer-bottom-section">
+
+            <div class="row wah-share-row">
+                <div class="large-6 columns">
+                    <div class="wah-share">
+                        <strong>שתפו אותנו ב:</strong>
+                        <div class="wah-share-buttons">
+                            <a href="https://www.facebook.com/sharer/sharer.php?u=https://accessibility-helper.co.il/il" target="_blank">
+                                Facebook
+                            </a>
+                            <a href="https://plus.google.com/share?url=https://accessibility-helper.co.il/il" target="_blank">
+                                Google+
+                            </a>
+                            <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=http%3A//accessibility-helper.co.il/il/&amp;title=WP%20Accessibility%20Helper%20-%20WordPress%20Accessibility%20made%20easy!&amp;summary=&amp;source=" target="_blank">
+                                LinkedIn
+                            </a>
+                            <a href="https://twitter.com/home?status=http%3A//accessibility-helper.co.il/il/%20-%20WP%20Accessibility%20Helper" target="_blank">
+                                Twitter
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="large-6 columns">
+                    <div class="wah-credits">
+                    WP Accessibility Helper נתמך על ידי <a href="http://volkov.co.il" target="_blank">אלכס וולקוב & WAH TEAM</a>
+                    <br>כל הזכויות שמורות | 2016 - 2017</div>
+                </div>
+            </div>
+
+        </div>
+
     </footer>
 </template>
 
